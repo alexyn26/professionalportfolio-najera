1068a06d9d97af20c3ce1a63ba86b9757f277dad
alexyn26 <107084291+alexyn26@users.noreply.github.com>
2022-12-04 12:09:45 -0600

    Create contact.js
    contact page

---
+++ professionalportfolio/src/components/Contact/contact.js
@@ -0,0 +1,43 @@
+
+import React from 'react';
+import Input from '@mui/material/Input';
+import FormControl from '@mui/material/FormControl';
+import InputLabel from '@mui/material/FormControl';
+
+
+
+import TextareaAutosize from '@mui/material/TextareaAutosize'
+
+const contact = () => {
+    return(
+        <>
+      
+       <h1>Contact Me</h1>
+       <FormControl>
+       <InputLabel htmlFor="name">Name</InputLabel>
+       <Input id="name" aria-describedby="my-helper-text" />
+       
+     </FormControl><br></br><br></br>
+        
+       <FormControl>
+       <InputLabel htmlFor="email">Email address</InputLabel>
+       <Input id="email" aria-describedby="my-helper-text" />
+     </FormControl>
+        <br></br><br></br>
+     
+      <InputLabel htmlFor = "message">Message:</InputLabel><br></br><br></br>
+     <TextareaAutosize
+        aria-label="minimum height"
+        minRows={3}
+         placeholder="How can I help you?"
+         style={{ width: 300 }}
+        />
+        
+
+
+       
+      </>
+    )
+    
+}
+export default contact;
