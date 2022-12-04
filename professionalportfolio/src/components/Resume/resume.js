64f6841313961b80c9cdfa768cfff7a5ec056467
alexyn26 <107084291+alexyn26@users.noreply.github.com>
2022-12-04 12:08:49 -0600

    Create resume.js
    resume page

---
+++ professionalportfolio/src/components/Resume/resume.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import resumephoto from '../../assets/resume.png';
+
+const Resume = () => {
+    return(
+        <>
+        <h1>Resume</h1>
+        <img src = {resumephoto} alt = 'resume'></img>
+        </>
+        
+
+    )
+    
+    
+}
+export default Resume;
