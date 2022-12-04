48b4c9bb76d35c01cfa8072084fc77b565382ff3
alexyn26 <107084291+alexyn26@users.noreply.github.com>
2022-12-04 12:10:55 -0600

    Create App.js
    app.js to render components

---
+++ professionalportfolio/src/App.js
@@ -0,0 +1,30 @@
+import Navbar from './components/Navbar';
+import About from './components/About';
+import Contact from './components/Contact';
+import Portfolio from './components/Portfolio';
+import Resume from './components/Resume';
+import Theme from './components/Theme';
+
+
+function App() {
+  
+  return (
+<>
+
+   
+   
+      <Navbar/>
+      <About/>
+      <Contact/>
+      
+      <Portfolio/>
+      <Resume/>
+
+      
+
+      </>
+    
+  );
+}
+
+export default App;
